2e0df0773813a52dd3f43c0253b2478a6f515e14
Abde Ali <iaabde1992@gmail.com>
2013-11-19 03:54:11 +0530

    a

--- Assets/Scripts/Voxel/Editor/VTEd.cs
+++ Assets/Scripts/Voxel/Editor/VTEd.cs
@@ -22,6 +22,7 @@ public class VTEd : Editor {
 	public override void OnInspectorGUI () {
 		v=target as VoxelTerrain;
 		DrawDefaultInspector ();
+		EditorGUILayout.BeginHorizontal ();
 		if(GUILayout.Button ("Init")) {
 			v.Init ();
 		}
@@ -48,31 +49,29 @@ public class VTEd : Editor {
 				}
 			}
 		}
+		EditorGUILayout.EndHorizontal ();
 		if(GUILayout.Button ("Make Part")) {
 			Undo.RegisterSceneUndo ("Make VT");
 			long t0=System.DateTime.Now.Ticks/System.TimeSpan.TicksPerMillisecond;
-			VTPart p=v.part(v.edx,v.edy);
-			string f1="/Part("+v.edx+","+v.edy+")";
-			EditorUtility.DisplayProgressBar("Making...","",0);
-			for(int i=0;i<v.lod.Length;i++) {
-				p.Make (v.lod[i].dn);
-				string f0="/Resources/VT/LOD"+i;
-				EditorUtility.DisplayProgressBar("Making...","",((float)i+0.5f)/(float)v.lod.Length);
-				if(!System.IO.Directory.Exists(Application.dataPath+f0+f1))
-					System.IO.Directory.CreateDirectory(Application.dataPath+f0+f1);
-				SavePart (v.edx,v.edy,f0+f1);
-				EditorUtility.DisplayProgressBar("Making...","",(float)(i+1)/(float)v.lod.Length);
-			}
-			v.part (v.edx,v.edy).MakeTrees ();
-			string f2="/Resources/VT/Trees";
-			if(!System.IO.Directory.Exists(Application.dataPath+f2+f1))
-				System.IO.Directory.CreateDirectory(Application.dataPath+f2+f1);
-			//SaveTrees (v.edx,v.edy,f2+f1);
+			MakePart (v.edx,v.edy,"Making "+v.edx+","+v.edy,0,1);
 			EditorUtility.ClearProgressBar ();
 			last=(System.DateTime.Now.Ticks/System.TimeSpan.TicksPerMillisecond-t0)/1000;
 		}
 		EditorGUILayout.LabelField ("Last Make Time:"+last);
+		EditorGUILayout.BeginHorizontal ();
+		mpn=EditorGUILayout.IntField ("N:",mpn);
+		if(GUILayout.Button ("Square")) {
+			int n=mpn;
+			for(int i=v.edx,m=0;i<v.edx+n;i++) {
+				for(int j=v.edy;j<v.edy+n;j++,m++) {
+					MakePart (i,j,"Making "+i+","+j,(float)m/(float)(n*n),(float)(m+1)/(float)(n*n));
+				}
+			}
+			EditorUtility.ClearProgressBar ();
+		}
+		EditorGUILayout.EndHorizontal ();
 	}
+	int mpn=3;
 	
 	void OnSceneGUI() {
 		v=target as VoxelTerrain;
@@ -107,11 +106,33 @@ public class VTEd : Editor {
 		Handles.EndGUI ();
 	}
 	
+	void MakePart(int x,int y,string s,float p0,float p1) {
+		VTPart p=v.part(x,y);
+		string f1="/Part("+x+","+y+")";
+		EditorUtility.DisplayProgressBar(s,"",p0);
+		for(int i=0;i<v.lod.Length;i++) {
+			p.Make (v.lod[i].dn);
+			string f0="/Resources/VT/LOD"+i;
+			EditorUtility.DisplayProgressBar(s,"",((float)i+0.5f)/(float)v.lod.Length*(p1-p0)+p0);
+			if(!System.IO.Directory.Exists(Application.dataPath+f0+f1))
+				System.IO.Directory.CreateDirectory(Application.dataPath+f0+f1);
+			SavePart (x,y,f0+f1);
+			EditorUtility.DisplayProgressBar("Making...","",(float)(i+1)/(float)v.lod.Length*(p1-p0)+p0);
+		}
+		p.MakeTrees ();
+		/*string f2="/Resources/VT/Trees";
+		if(!System.IO.Directory.Exists(Application.dataPath+f2+f1))
+			System.IO.Directory.CreateDirectory(Application.dataPath+f2+f1);
+		SaveTrees (v.edx,v.edy,f2+f1);*/
+	}
+	
 	public void SavePart(int x,int y,string path) {
 		VTPart vp=v.part(x,y);
 		foreach(VTChunk c in vp.chunks) {
 			string p=path+"/"+c.gameObject.name+".asset";
 			if(System.IO.File.Exists(Application.dataPath+p)) System.IO.File.Delete (Application.dataPath+p);
+			if(AssetDatabase.LoadMainAssetAtPath("Assets"+p)!=null)
+				AssetDatabase.DeleteAsset ("Assets"+p);
 			AssetDatabase.CreateAsset (c.GetComponent<MeshFilter>().sharedMesh,"Assets"+p);
 			AssetDatabase.SaveAssets ();
 			AssetDatabase.Refresh ();
